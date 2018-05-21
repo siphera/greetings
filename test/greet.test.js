0bcb47b5f0620726bdc9468939b5efa6c77dc590
siphera <salmansiphenkosi@gmail.com>
2018-05-21 17:35:40 +0200

    test

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,7 +1,19 @@
 describe("greet function", function() {
-    it("it should greet with selected language", function() {
-        var greeting = greetings({});
+    it("it should return the name typed in", function() {
+        var greeting = greetings();
+        greeting.names("siphenkosi");
+        assert.equal(greeting.getName(), "siphenkosi");
+    });
 
-        
-    })
-})
+    
+    it("It should Greet the user in english", function() {
+        var greeting = greetings();
+        greeting.names("siphenkosi")
+        greeting.language("english");
+        assert.equal(greeting.outputMsg(), "Hello, siphenkosi");
+    });
+    
+    
+    
+    
+});    
